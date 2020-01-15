3094a0b35df0f6ca773b4a5d94264052ad3e40a6
AbiePerdanaKusuma1054 <44666830+AbiePerdanaKusuma1054@users.noreply.github.com>
2020-01-15 12:58:30 +0700

    Macam-macam Looping

---
+++ kakpatar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	for(int i=1;i<=5;i++)cout<<i;
+	////////
+	int i=1;
+	while(i<=5);
+	i++;
+	cout<<i;
+	////////
+	do{
+	int i=1;
+	cout<<i;
+	i++;
+	}	
+	while(i<=5);
+	////////
+	
+}
